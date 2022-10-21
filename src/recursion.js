d16b00cb7d4dc463181bea7cfe6dde192b832946
Colin Fiedorowicz <70108727+colin-intj@users.noreply.github.com>
2022-10-21 16:02:54 -0500

    Use `let` instead of `var`

--- src/recursion.js
+++ src/recursion.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 6 */
 
-/* eslint-disable no-var, no-unused-vars, prefer-const */
+/* eslint-disable no-unused-vars, prefer-const */
 
 // Solve the following prompts using recursion.
 
@@ -8,32 +8,32 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
-var factorial = (n) => {
+let factorial = (n) => {
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
-var sum = (array) => {
+let sum = (array) => {
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = (array) => {
+let arraySum = (array) => {
 };
 
 // 4. Check if a number is even.
-var isEven = (n) => {
+let isEven = (n) => {
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
-var sumBelow = (n) => {
+let sumBelow = (n) => {
 };
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
-var range = (x, y) => {
+let range = (x, y) => {
 };
 
 // 7. Compute the exponent of a number.
@@ -41,22 +41,22 @@ var range = (x, y) => {
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
-var exponent = (base, exp) => {
+let exponent = (base, exp) => {
 };
 
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
-var powerOfTwo = (n) => {
+let powerOfTwo = (n) => {
 };
 
 // 9. Write a function that reverses a string.
-var reverse = (string) => {
+let reverse = (string) => {
 };
 
 // 10. Write a function that determines if a string is a palindrome.
-var palindrome = (string) => {
+let palindrome = (string) => {
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -64,17 +64,17 @@ var palindrome = (string) => {
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
-var modulo = (x, y) => {
+let modulo = (x, y) => {
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
-var multiply = (x, y) => {
+let multiply = (x, y) => {
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
-var divide = (x, y) => {
+let divide = (x, y) => {
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
@@ -82,29 +82,29 @@ var divide = (x, y) => {
 // gcd(4,36); // 4
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
-var gcd = (x, y) => {
+let gcd = (x, y) => {
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
 // both are identical.
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
-var compareStr = (str1, str2) => {
+let compareStr = (str1, str2) => {
 };
 
 // 16. Write a function that accepts a string and creates an array where each varter
 // occupies an index of the array.
-var createArray = (str) => {
+let createArray = (str) => {
 };
 
 // 17. Reverse the order of an array
-var reverseArr = (array) => {
+let reverseArr = (array) => {
 };
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
-var buildList = (value, length) => {
+let buildList = (value, length) => {
 };
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
@@ -112,37 +112,37 @@ var buildList = (value, length) => {
 // For multiples of five, output 'Buzz' instead of the number.
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
-var fizzBuzz = (n) => {
+let fizzBuzz = (n) => {
 };
 
 // 20. Count the occurrence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
-var countOccurrence = (array, value) => {
+let countOccurrence = (array, value) => {
 };
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
-var rMap = (array, callback) => {
+let rMap = (array, callback) => {
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
-var countKeysInObj = (obj, key) => {
+let countKeysInObj = (obj, key) => {
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
 // countValuesInObj(obj, 'r') // 2
 // countValuesInObj(obj, 'e') // 1
-var countValuesInObj = (obj, value) => {
+let countValuesInObj = (obj, value) => {
 };
 
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
-var replaceKeysInObj = (obj, oldKey, newKey) => {
+let replaceKeysInObj = (obj, oldKey, newKey) => {
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
@@ -150,7 +150,7 @@ var replaceKeysInObj = (obj, oldKey, newKey) => {
 // Example: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.....
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
-var fibonacci = (n) => {
+let fibonacci = (n) => {
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -158,18 +158,18 @@ var fibonacci = (n) => {
 // nthFibo(5); // 5
 // nthFibo(7); // 13
 // nthFibo(3); // 2
-var nthFibo = (n) => {
+let nthFibo = (n) => {
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
-var capitalizeWords = (array) => {
+let capitalizeWords = (array) => {
 };
 
 // 28. Given an array of strings, capitalize the first varter of each index.
 // capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
-var capitalizeFirst = (array) => {
+let capitalizeFirst = (array) => {
 };
 
 // 29. Return the sum of all even numbers in an object containing nested objects.
@@ -181,17 +181,17 @@ var capitalizeFirst = (array) => {
 //   e: {e: {e: 2}, ee: 'car'}
 // };
 // nestedEvenSum(obj1); // 10
-var nestedEvenSum = (obj) => {
+let nestedEvenSum = (obj) => {
 };
 
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
-var flatten = (array) => {
+let flatten = (array) => {
 };
 
 // 31. Given a string, return an object containing tallies of each varter.
 // varterTally('potato'); // {p:1, o:2, t:2, a:1}
-var varterTally = (str, obj) => {
+let varterTally = (str, obj) => {
 };
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
@@ -199,51 +199,51 @@ var varterTally = (str, obj) => {
 // elements should not be changed.
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
-var compress = (list) => {
+let compress = (list) => {
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
-var augmentElements = (array, aug) => {
+let augmentElements = (array, aug) => {
 };
 
 // 34. Reduce a series of zeroes to a single 0.
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
-var minimizeZeroes = (array) => {
+let minimizeZeroes = (array) => {
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
 // their original sign. The first number in the index always needs to be positive.
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
-var alternateSign = (array) => {
+let alternateSign = (array) => {
 };
 
 // 36. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = (str) => {
+let numToText = (str) => {
 };
 
 // *** EXTRA CREDIT ***
 
 // 37. Return the number of times a tag occurs in the DOM.
-var tagCount = (tag, node) => {
+let tagCount = (tag, node) => {
 };
 
 // 38. Write a function for binary search.
 // var array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 // binarySearch(array, 5) // 5
 // https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
-var binarySearch = (array, target, min, max) => {
+let binarySearch = (array, target, min, max) => {
 };
 
 // 39. Write a merge sort function.
 // mergeSort([34,7,23,32,5,62]) // [5,7,23,32,34,62]
 // https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/divide-and-conquer-algorithms
-var mergeSort = (array) => {
+let mergeSort = (array) => {
 };
 
 // 40. Deeply clone objects and arrays.
@@ -251,5 +251,5 @@ var mergeSort = (array) => {
 // var obj2 = clone(obj1);
 // console.log(obj2); // {a:1,b:{bb:{bbb:2}},c:3}
 // obj1 === obj2 // false
-var clone = (input) => {
+let clone = (input) => {
 };
